7539bfa8a7841a7a24ee2e06cb1ab24af6934076
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-08-04 05:38:36 +0000

    modified the include statement for getAppParameters.php to get-parameters.php

--- menu.php
+++ menu.php
@@ -16,7 +16,7 @@
 	<br>
 	<?php
 		// Get the application environment parameters from the Parameter Store.
-		include ('getAppParameters.php');
+		include ('get-parameters.php');
 
 		// Display the server metadata information if the showServerInfo parameter is true.
 		include('serverInfo.php');
@@ -28,12 +28,9 @@
 		<a href="query.php" class="active">Query</a>
 	</div>
 
-
-
 	<div id="Copyright" class="center">
 		<h5>&copy; 2020, Amazon Web Services, Inc. or its Affiliates. All rights reserved.</h5>
 	</div>
-
 
 </body>
 </html>
